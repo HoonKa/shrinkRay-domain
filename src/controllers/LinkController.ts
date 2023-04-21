99eda98abd791119789f112d3274e05bae5e3b4c
HoonKa <younghoo.kang@smail.astate.edu>
2023-04-21 17:44:29 +0900

    update

---
+++ src/controllers/LinkController.ts
@@ -0,0 +1,105 @@
+import { Request, Response } from 'express';
+import { parseDatabaseError } from '../utils/db-utils';
+import {
+  createNewLink,
+  getLinksByUserId,
+  updateLinkVisits,
+  createLinkId,
+  getLinkById,
+  getLinksByUserIdForOwnAccount,
+  deleteLink,
+} from '../models/LinkModel';
+import { getUserById } from '../models/UserModel';
+
+async function shortenUrl(req: Request, res: Response): Promise<void> {
+  if (!req.session.isLoggedIn) {
+    res.sendStatus(403);
+    return;
+  }
+
+  // Get the userId from `req.session`
+  const { authenticatedUser } = req.session;
+
+  const user = await getUserById(authenticatedUser.userId);
+  if (!user) {
+    res.sendStatus(404);
+    return;
+  }
+
+  if ((!user.isPro || !user.isAdmin) && user.links.length >= 5) {
+    res.sendStatus(403);
+    return;
+  }
+
+  const { originalUrl } = req.body as NewLinkRequest;
+
+  const linkId = createLinkId(originalUrl, user.userId);
+
+  try {
+    const newLink = await createNewLink(originalUrl, linkId, user);
+    newLink.user.passwordHash = undefined;
+    res.redirect(`/api/users/${user.userId}/links`);
+  } catch (err) {
+    console.error(err);
+    const databaseErrorMessage = parseDatabaseError(err);
+    res.status(500).json(databaseErrorMessage);
+  }
+}
+
+async function visitLink(req: Request, res: Response): Promise<void> {
+  const { targetLinkId } = req.params as LinkParam;
+
+  const link = await getLinkById(targetLinkId);
+  if (!link) {
+    res.sendStatus(404);
+    return;
+  }
+
+  await updateLinkVisits(link);
+
+  res.redirect(301, link.originalUrl);
+}
+
+async function getLinks(req: Request, res: Response): Promise<void> {
+  const { targetUserId } = req.params as UserIdParam;
+
+  let links;
+  if (!req.session.isLoggedIn || req.session.authenticatedUser.userId !== targetUserId) {
+    links = await getLinksByUserId(targetUserId);
+  } else {
+    links = await getLinksByUserIdForOwnAccount(targetUserId);
+  }
+
+  const targetUser = await getUserById(targetUserId);
+  const { protocol } = req;
+  const host = req.get('host');
+  const url = `${protocol}://${host}`;
+
+  res.render('linksPage', { targetUser, links, url });
+}
+
+async function removeLink(req: Request, res: Response): Promise<void> {
+  const { targetUserId, targetLinkId } = req.params as DeleteLinkRequest;
+  const { isLoggedIn, authenticatedUser } = req.session;
+  if (!isLoggedIn) {
+    res.sendStatus(401);
+    return;
+  }
+
+  const link = await getLinkById(targetLinkId);
+  if (!link) {
+    res.sendStatus(404);
+    return;
+  }
+
+  if (authenticatedUser.userId !== targetUserId && !authenticatedUser.isAdmin) {
+    res.sendStatus(403);
+    return;
+  }
+
+  await deleteLink(targetLinkId);
+
+  res.redirect(`/api/users/${targetUserId}/links`);
+}
+
+export { shortenUrl, visitLink, getLinks, removeLink };
